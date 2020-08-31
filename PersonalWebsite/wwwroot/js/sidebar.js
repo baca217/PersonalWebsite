ac06d27c091661b6705e53976bf3caac8566242b
Elmer Baca Holguin <baca217@gmail.com>
2020-08-30 23:28:09 -0600

    Added dimmer for when sidebar opens. Fixed sidebar button not collapsing with the sidebar.

--- PersonalWebsite/wwwroot/js/sidebar.js
+++ PersonalWebsite/wwwroot/js/sidebar.js
@@ -1,5 +1,7 @@
 ﻿$(document).ready(function () {
     $('#sidebarCollapse').on('click', function() {
         $('#sidebar').toggleClass('active');
+        $('#dimmer').toggleClass('active');
+        $('#sidebarCollapse').toggleClass('active');
     });
 });
