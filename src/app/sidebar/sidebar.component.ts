4d4961d10121652c226324095202d9ffaaa2e153
baca217 <baca217@gmail.com>
2021-12-23 23:14:19 -0700

    added a navbar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -13,48 +13,4 @@ export class SidebarComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  openNav(){
-    var sidebar = document.getElementById("mySidebar");
-    var main = document.getElementById("main");
-    var blackout = document.getElementById("blackout");
-
-    if(sidebar){sidebar.style.width = "250px";}
-    if(main){main.style.width = "250px";}
-    if(blackout){
-      blackout.style.transition = "0s";
-      blackout.style.height = "100%";
-      blackout.style.width = "100%";
-      blackout.style.zIndex = "98";
-      waitForElementTransition(blackout).then(() => {
-        if(blackout)
-        {
-          blackout.style.transition = "1s";
-          blackout.style.opacity = "20%";
-        }
-
-      })
-      
-    }
-  }
-
-  closeNav(){
-    var sidebar = document.getElementById("mySidebar");
-    var main = document.getElementById("main");
-    var blackout = document.getElementById("blackout");
-
-    if(sidebar){sidebar.style.width = "0";}
-    if(main){main.style.width = "0";}
-    if(blackout){
-      blackout.style.transition = "0.2s";
-      blackout.style.opacity = "0%"
-      blackout.style.zIndex = "0";
-      waitForElementTransition(blackout).then(() => {
-        if(blackout){
-          blackout.style.width = "0%";
-          blackout.style.height = "0%";
-        }
-      })
-    }
-
-  }
 }
