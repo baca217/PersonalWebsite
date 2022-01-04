93e7904f3615e63c1eeb41a4dc506b950f39e0a1
baca217 <baca217@gmail.com>
2022-01-03 20:24:28 -0700

    added some skeleton to hobbies page

--- src/app/hobbies/hobbies.component.ts
+++ src/app/hobbies/hobbies.component.ts
@@ -12,4 +12,28 @@ export class HobbiesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  getBusterAge() {
+    let diff = new Date()
+    let busterAge = new Date();
+    busterAge.setFullYear(2021);
+    busterAge.setMonth(5);
+    busterAge.setDate(25);
+
+    diff.setFullYear(diff.getFullYear() - busterAge.getFullYear());
+    diff.setMonth(diff.getMonth() - busterAge.getMonth());
+    diff.setDate(diff.getDate() - busterAge.getDate());
+    console.log(diff.getFullYear());
+    let ret = "";
+
+    if (diff.getFullYear() > 0) {
+      if(diff.getFullYear() > 1){ret += diff.getFullYear() + "year old and ";}
+      else{ret += diff.getFullYear() + "years old and ";}
+    }
+    if (diff.getMonth() > 0) {
+      if(diff.getMonth() > 1){ret += diff.getMonth() + " months old";}
+      else{ret += diff.getMonth() + " month old";}
+    }
+    return ret;
+  }
+
 }
